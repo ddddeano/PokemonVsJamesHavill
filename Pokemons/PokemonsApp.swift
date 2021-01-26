4d4367b4864e40dcf5bc151703dbcdd1e7fe00cc
Daniel Watson <55237431+ddddeano@users.noreply.github.com>
2021-01-26 22:45:26 +0100

    Add files via upload

---
+++ Pokemons/PokemonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsApp.swift
+//  Pokemons
+//
+//  Created by Daniel Watson on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
